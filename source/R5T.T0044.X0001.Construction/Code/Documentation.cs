37241b0eecfb6422cb0a6a26d240c3435ff0cb79
davidcoats <david@getsafetycone.com>
2022-01-03 00:27:09 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0044.X0001.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0044.X0001.Construction
+{
+    /// <summary>
+    /// Construction project for Visual Studio related IFileSystemOperator extensions..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0044.X0001/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0044.X0001
+{
+    /// <summary>
+    /// Visual Studio related IFileSystemOperator extensions.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
