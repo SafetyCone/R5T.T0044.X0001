63222cbf1bd88320070d11c45cc550fdef49c4be
David Coats <david@getsafetycone.com>
2022-01-05 12:36:35 -0800

    Initial implementation.

---
+++ source/R5T.T0044.X0001/Code/Bases/Extensions/IFileSystemOperatorExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+using R5T.T0044;
+
+using Instances = R5T.T0044.X0001.Instances;
+
+
+namespace System
+{
+    public static class IFileSystemOperatorExtensions
+    {
+        public static IEnumerable<string> FindSolutionFilesInDirectoryOrDirectParentDirectories(this IFileSystemOperator _,
+            string directoryPath)
+        {
+            var output = _.FindFilesInDirectoryOrDirectParentDirectories(
+                directoryPath,
+                Instances.SearchPattern.AllSolutionFiles());
+
+            return output;
+        }
+
+        public static IEnumerable<string> FindSolutionFilesInFileDirectoryOrDirectParentDirectories(this IFileSystemOperator _,
+            string filePath)
+        {
+            var directoryPath = Instances.PathOperator.GetDirectoryPathOfFilePath(filePath);
+
+            var output = _.FindSolutionFilesInDirectoryOrDirectParentDirectories(
+                directoryPath);
+
+            return output;
+        }
+    }
+}
---
+++ source/R5T.T0044.X0001/Code/Instances.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using R5T.T0041;
+using R5T.T0124;
+
+
+namespace R5T.T0044.X0001
+{
+    public static class Instances
+    {
+        public static IPathOperator PathOperator { get; } = T0041.PathOperator.Instance;
+        public static ISearchPattern SearchPattern { get; } = T0124.SearchPattern.Instance;
+    }
+}
